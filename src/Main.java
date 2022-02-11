36e735bfcdc3554475679ba347d226137e7e140f
SergeyStrelnikovv <94080285+SergeyStrelnikovv@users.noreply.github.com>
2022-02-11 18:25:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
         public static void main(String[] args) {
             int pay = 1000;
             int balance = 100;
-            int bonus = pay / 100;
+            int bonus;
 
             if (pay > 1000){
                 bonus = pay / 100;
